7c9bc962a5356e69abd78da32f01b057c8a14351
Lena <tsybulenko.helen@gmail.com>
2020-02-07 12:44:56 +0300

    Update index.js
    solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) {	
+  	return Math.min(...array);
+  }
+  else {
+  	return 0;	  
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {	
+  	return Math.max(...array);
+  }
+  else {
+  	return 0;	  
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array && array.length > 0) {		
+		var sum  = array.reduce(function(prev, current) {
+			return prev + current;
+		 });
+		 
+		return sum/array.length; 
+	}
+	
+	else {
+		 
+		return 0; 
+    }
 }
